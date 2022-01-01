06f976e051bc51a1f61551d88547dd1807ab1b7c
johannpereze <sebas.jsp@gmail.com>
2022-01-01 15:08:20 -0500

    Class 173. second router created for the dashboard

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,20 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { DcScreen } from "../components/dc/DcScreen"
 import { LoginScreen } from "../components/login/LoginScreen"
-import { MarvelScreen } from "../components/marvel/MarvelScreen"
-import { SearchScreen } from "../components/search/SearchScreen"
-import { Navbar } from "../components/ui/NavBar"
+import { DashboardRoutes } from "./DashboardRoutes"
 
 export const AppRouter = () => {
     return (
         <BrowserRouter>
-            <Navbar />
             <Routes>
-                <Route path="/" element={<MarvelScreen />} />
-                <Route path="marvel" element={<MarvelScreen />} />
-                <Route path="dc" element={<DcScreen />} />
-                <Route path="search" element={<SearchScreen />} />
                 <Route path="login" element={<LoginScreen />} />
+                <Route path="/*" element={<DashboardRoutes />} />
             </Routes>
         </BrowserRouter>
     )
