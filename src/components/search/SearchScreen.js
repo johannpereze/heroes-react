09339c6638c666bf739a9b04f1d221cde5e21322
johannpereze <sebas.jsp@gmail.com>
2022-01-14 11:17:56 -0500

    Clase 181. Usamos el custom hook useForm para controlar el input del searchScreen

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,7 +1,47 @@
+import { useForm } from "../../hooks/useForm";
+
 export const SearchScreen = () => {
+
+    const initialForm = { searchText: "" };
+
+    const [formValue, handleInputChange, reset] = useForm(initialForm);
+
+    const { searchText } = formValue;
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        console.log(formValue.searchText);
+        reset()
+    }
+
     return (
-        <div>
-            <h1>Search Screen</h1>
-        </div>
+        <>
+            <h1>Búsquedas</h1>
+            <hr />
+
+            <div className="row">
+                <div className="col-5">
+                    <h4>Buscar</h4>
+                    <hr />
+                    <form onSubmit={handleSearch}>
+                        <input
+                            type="text"
+                            placeholder="Buscar un héroe..."
+                            className="form-control"
+                            name="searchText"
+                            autoComplete="off"
+                            onChange={handleInputChange}
+                            value={searchText}
+                        />
+                        <button
+                            type="submit"
+                            className="btn btn-outline-primary mt-3"
+                        >
+                            Buscar
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </>
     )
 }
