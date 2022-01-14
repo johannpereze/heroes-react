6adf93d9c115d10cd730d3e337830e0dce4be90e
johannpereze <sebas.jsp@gmail.com>
2022-01-14 10:01:33 -0500

    Clase 178 y 179 creamos la página del héroe, memoizamos las funciones y animamos 2 páginas

--- src/components/hero/HeroList.js
+++ src/components/hero/HeroList.js
@@ -1,12 +1,13 @@
+import { useMemo } from "react"
 import { getHeroesByPublisher } from "../../selectors/getHeroesByPublisher"
 import { HeroCard } from "./HeroCard"
 
 export const HeroList = ({ publisher }) => {
 
-    const heroes = getHeroesByPublisher(publisher)
+    const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher])
 
     return (
-        <div className="row row-cols-1 row-cols-md-3 g-3">
+        <div className="row row-cols-1 row-cols-md-3 g-3 animate__animated animate__fadeIn">
             {
                 heroes.map((hero) => <HeroCard
                     key={hero.id}
--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,4 +1,5 @@
-import { Navigate, useParams } from "react-router-dom"
+import { useMemo } from "react"
+import { Navigate, useNavigate, useParams } from "react-router-dom"
 import { getHeroesById } from "../../selectors/getHeroesById"
 
 export const HeroScreen = () => {
@@ -6,7 +7,9 @@ export const HeroScreen = () => {
     // Get the heroeId from the URL
     const { heroeId } = useParams()
 
-    const heroe = getHeroesById(heroeId)
+    const heroe = useMemo(() => getHeroesById(heroeId), [heroeId])
+
+    const navigate = useNavigate()
 
     // If the heroeId is not found, redirect to the Marvel screen. This way we return a react functional component
     if (!heroe) {
@@ -22,11 +25,37 @@ export const HeroScreen = () => {
         first_appearance,
         characters } = heroe
 
+    const handleReturn = () => {
+        navigate(-1)
+    }
+
+    const imagePath = `/assets/${id}.jpg`
+
     return (
-        <div>
-            <h1>Hero Screen</h1>
-            <h2>{superhero}</h2>
-            <h2>{alter_ego}</h2>
+        <div className="row mt-5 animate__animated animate__fadeIn">
+            <div className="col-4">
+                <img
+                    src={imagePath}
+                    alt={superhero}
+                    className="img-thumbnail"
+                />
+            </div>
+            <div className="col-8">
+                <h3>{superhero}</h3>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"><b>Alter ego:</b> {alter_ego} </li>
+                    <li className="list-group-item"><b>Publisher:</b> {publisher} </li>
+                    <li className="list-group-item"><b>First Apperance:</b> {first_appearance} </li>
+                    <h5 className="mt-3">Characters</h5>
+                    <p>{characters}</p>
+                    <button
+                        className="btn btn-outline-info"
+                        onClick={handleReturn}
+                    >
+                        Regresar...
+                    </button>
+                </ul>
+            </div>
         </div>
     )
 }
