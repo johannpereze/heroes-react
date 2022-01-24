cb591693e39c76866590f80b05aab9acd757d8ec
johannpereze <sebas.jsp@gmail.com>
2022-01-24 16:10:07 -0500

    clase 191. Creamos el context y el reducer

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import { AuthContext } from './auth/authContext'
+import { authReducer } from './auth/authReducer'
 import { AppRouter } from './routers/AppRouter'
 
 export const HeroesApp = () => {
 
+    const init = () => {
+        return JSON.parse(localStorage.getItem('user')) || { logged: false }
+    }
+
+    const [user, dispatch] = useReducer(authReducer, {}, init);
+
+
     return (
-        <AppRouter />
+        <AuthContext.Provider value={{
+            user,
+            dispatch
+        }}>
+            <AppRouter />
+        </AuthContext.Provider>
     )
 }
--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link, NavLink, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../auth/authContext';
 
 export const Navbar = () => {
+
+    const { user } = useContext(AuthContext);
+    const { name } = user;
+
 
     const navigate = useNavigate()
 
@@ -42,7 +47,7 @@ export const Navbar = () => {
             </div>
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
-                    <span className='nav-item nav-link text-info '>Johann</span>
+                    <span className='nav-item nav-link text-info '>{name}</span>
                     <button
                         className="nav-item nav-link btn "
                         onClick={handleLogout}
